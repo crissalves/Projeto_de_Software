e884864542255cd6ef9c314ff6a5918f24c8de10
crissalves <cristianalves_cg@hotmail.com>
2023-04-01 12:30:01 -0300

    Pratica 3. Implementada Repository e service para os testes.

---
+++ Praticas/mercadofacil/src/main/java/com/ufcg/psoft/mercadofacil/service/ProdutoAlterarImplService.java
@@ -0,0 +1,21 @@
+package com.ufcg.psoft.mercadofacil.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ufcg.psoft.mercadofacil.model.Produto;
+import com.ufcg.psoft.mercadofacil.repository.ProdutoRepository;
+
+@Service
+public class ProdutoAlterarImplService implements ProdutoAlterarService {
+
+    @Autowired
+    ProdutoRepository<Produto, Long> produtoRepository;
+    @Override
+    public Produto alterar (Produto produtoAlterado) {
+        if(produtoAlterado.getPreco()<=0) {
+            throw new RuntimeException("Preco invalido");
+        }
+        return produtoRepository.update(produtoAlterado);
+    }
+}
